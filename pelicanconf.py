76c35a508aeef0924ac3c1994ca38f3de0e3b09c
vishalchopra666 <digital.bill85@protonmail.com>
2024-09-24 23:10:15 +0530

    config

--- pelicanconf.py
+++ pelicanconf.py
@@ -79,5 +79,5 @@ SITEMAP = {
 
 PLUGINS = ['minify','sitemap']
 
-# Uncomment following line if you want document-relative URLs when developing
+# Uncomment following line if you want document-relative URLs when developing 
 # RELATIVE_URLS =  True
